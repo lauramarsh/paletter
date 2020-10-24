69b038746c89ac522d236a45a99fac7dd692cf19
Laura <lau.lyn.marsh@gmail.com>
2020-10-23 21:18:49 -0700

    OK removed nested git tracking in the UI >:)

---
+++ ui_paletter/src/components/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>{{ helloMsg }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  props: {
+    helloMsg: String,
+  },
+};
+</script>
+
---
+++ ui_paletter/src/views/Landing.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="MainLanding">
+    <Main helloMsg="For Paletter" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Main from "@/components/Main.vue";
+
+export default {
+  name: "MainLanding",
+  components: {
+    Main
+  }
+};
+</script>
