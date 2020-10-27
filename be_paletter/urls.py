e06e566ce2cae2d7c19ad4a165af10db16b83e2a
Laura <lau.lyn.marsh@gmail.com>
2020-10-26 18:59:31 -0700

    SERIALIZING GIFS PROPERLY BAYBEE:

--- be_paletter/urls.py
+++ be_paletter/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from be_paletter import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('palettes/', views.palettes_list),
     path('palettes/<int:pk>/', views.palettes_detail),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns = format_suffix_patterns(urlpatterns)
